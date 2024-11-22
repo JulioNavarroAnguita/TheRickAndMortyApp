dad0003e8b16ac624f0f46019ac2fdf24bd5ca0d
Julio Navarro Anguita <julio.navarro.anguita@gmail.com>
2024-11-22 13:50:24 +0100

    change app icon and readme (#3)

--- presentation-layer/src/main/kotlin/com/example/presentation_layer/ui/theme/Theme.kt
+++ presentation-layer/src/main/kotlin/com/example/presentation_layer/ui/theme/Theme.kt
@@ -10,10 +10,12 @@ import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 
 private val DarkColorScheme = darkColorScheme(
     primary = Purple80,
@@ -25,16 +27,6 @@ private val LightColorScheme = lightColorScheme(
     primary = Purple40,
     secondary = PurpleGrey40,
     tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
 )
 
 @Composable
@@ -53,11 +45,20 @@ fun TheRickAndMortyAppTheme(
         darkTheme -> DarkColorScheme
         else -> LightColorScheme
     }
+    val systemUiController = rememberSystemUiController()
+    if (darkTheme){
+        systemUiController.setSystemBarsColor(
+            color = MaterialTheme.colorScheme.background
+        )
+    } else{
+        systemUiController.setSystemBarsColor(
+            color = MaterialTheme.colorScheme.onBackground
+        )
+    }
     val view = LocalView.current
     if (!view.isInEditMode) {
         SideEffect {
             val window = (view.context as Activity).window
-            window.statusBarColor = colorScheme.background.toArgb()
             WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
         }
     }
